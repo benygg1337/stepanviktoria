8c45a87ba7b9a99f5d3c28dc3dd3b9aa42724086
Ваше Имя <kozin@inetstudio.ru>
2024-12-02 02:19:43 +0300

    fix_popup

--- footer.php
+++ footer.php
@@ -1,14 +1,42 @@
 
 <?php wp_footer(); ?>
 
-<div class="popup popup_thanks">
-    <div class="popup__content">
-        <div class="popup__body">
-            <div class="popup__close"></div>
-            <div class="thanks__title">Спасибо</div>
-            <div class="thanks__text"><br>Анкета отправлена!<br>
-                 </div>
+<div class="popup popup_in10-fashion">
+        <div class="popup__content">
+            <div class="popup__body _gallery">
+                <div class="popup__close"></div>
+                <a href="img/invitation_no_10/fashion01.jpg" class="in10-fashion__image-fashion">
+                    <img src="img/invitation_no_10/fashion01.jpg" alt="">
+                </a>
+                <a href="img/invitation_no_10/fashion02.jpg" class="in10-fashion__image-fashion">
+                    <img src="img/invitation_no_10/fashion02.jpg" alt="">
+                </a>
+                <a href="img/invitation_no_10/fashion03.jpg" class="in10-fashion__image-fashion">
+                    <img src="img/invitation_no_10/fashion03.jpg" alt="">
+                </a>
+                <a href="img/invitation_no_10/fashion04.jpg" class="in10-fashion__image-fashion">
+                    <img src="img/invitation_no_10/fashion04.jpg" alt="">
+                </a>
+                <a href="img/invitation_no_10/fashion05.jpg" class="in10-fashion__image-fashion">
+                    <img src="img/invitation_no_10/fashion05.jpg" alt="">
+                </a>
+                <a href="img/invitation_no_10/fashion06.jpg" class="in10-fashion__image-fashion">
+                    <img src="img/invitation_no_10/fashion06.jpg" alt="">
+                </a>
+                <a href="img/invitation_no_10/fashion07.jpg" class="in10-fashion__image-fashion">
+                    <img src="img/invitation_no_10/fashion07.jpg" alt="">
+                </a>
+            </div>
         </div>
     </div>
-</div>
+    <div class="popup popup_thanks">
+        <div class="popup__content">
+            <div class="popup__body">
+                <div class="popup__close"></div>
+                <div class="thanks__title in10-title">Спасибо!</div>
+                <div class="thanks__text">Ваше мнение важно для нас.<br>
+                </div>
+            </div>
+        </div>
+    </div>
 </html>
--- index.php
+++ index.php
@@ -698,43 +698,6 @@ $query = new WP_Query($args);
         </div>
     </div>
 </body>
-<div class="popup popup_in10-fashion">
-        <div class="popup__content">
-            <div class="popup__body _gallery">
-                <div class="popup__close"></div>
-                <a href="img/invitation_no_10/fashion01.jpg" class="in10-fashion__image-fashion">
-                    <img src="img/invitation_no_10/fashion01.jpg" alt="">
-                </a>
-                <a href="img/invitation_no_10/fashion02.jpg" class="in10-fashion__image-fashion">
-                    <img src="img/invitation_no_10/fashion02.jpg" alt="">
-                </a>
-                <a href="img/invitation_no_10/fashion03.jpg" class="in10-fashion__image-fashion">
-                    <img src="img/invitation_no_10/fashion03.jpg" alt="">
-                </a>
-                <a href="img/invitation_no_10/fashion04.jpg" class="in10-fashion__image-fashion">
-                    <img src="img/invitation_no_10/fashion04.jpg" alt="">
-                </a>
-                <a href="img/invitation_no_10/fashion05.jpg" class="in10-fashion__image-fashion">
-                    <img src="img/invitation_no_10/fashion05.jpg" alt="">
-                </a>
-                <a href="img/invitation_no_10/fashion06.jpg" class="in10-fashion__image-fashion">
-                    <img src="img/invitation_no_10/fashion06.jpg" alt="">
-                </a>
-                <a href="img/invitation_no_10/fashion07.jpg" class="in10-fashion__image-fashion">
-                    <img src="img/invitation_no_10/fashion07.jpg" alt="">
-                </a>
-            </div>
-        </div>
-    </div>
-    <div class="popup popup_thanks">
-        <div class="popup__content">
-            <div class="popup__body">
-                <div class="popup__close"></div>
-                <div class="thanks__title in10-title">Спасибо!</div>
-                <div class="thanks__text">Ваше мнение важно для нас.<br>
-                </div>
-            </div>
-        </div>
-    </div>
+
 <?php
 get_footer(); ?>
